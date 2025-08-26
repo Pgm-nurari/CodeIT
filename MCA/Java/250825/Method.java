f15ea739ea24d941fa694f5c9c89d9f84bbde88e
Sabhyam Mishra <111678228+Pgm-nurari@users.noreply.github.com>
2025-08-26 07:35:19 +0530

    final commit of 25th  august

---
+++ MCA/Java/250825/Method.java
@@ -0,0 +1,14 @@
+class myFunction{
+    void display(){
+        System.out.println("\n Hello this is a display function...!");
+    }
+}
+
+
+
+public class Method extends myFunction{
+    public static void main(String[] args) {
+        // myFunction obj = new myFunction();
+        // obj.display();
+    }
+}
