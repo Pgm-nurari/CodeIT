d9e3eaf3b3324f7bec0d51fa9421171b611852bb
Sabhyam Mishra <111678228+Pgm-nurari@users.noreply.github.com>
2025-10-05 21:56:00 +0530

    java class works and assignments updated as per dates

--- MCA/Java/250915/Quest1.java
+++ MCA/Java/250915/Quest1.java
@@ -1,11 +1,4 @@
-class Person{
-    String fname;
-    String lname;
-
-    void setName(String firstName, String lastName){
-        fname = firstName;
-        lname = lastName;
-    }
-}
-
-// class...
+/*
+ * 
+ * 
+ */
